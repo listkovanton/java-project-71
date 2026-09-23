c85c84c488dcffa29b75ffe8523524d2f7fb966a
alistkov <alistkov.dev@gmail.com>
2026-09-23 12:37:09 +0200

    install pacjages update plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    id("io.github.ben-manes.versions") version "0.64.0"
 }
 
 group = "hexlet.code"
@@ -10,11 +10,4 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
