8be1a3e2cae1226252ac70027053d82c2dd79d52
alistkov <alistkov.dev@gmail.com>
2026-09-24 23:33:49 +0200

    add information about params and options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import picocli.CommandLine;import picocli.CommandLine.Command;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Command;
+
 import java.util.concurrent.Callable;
 
 @Command(
@@ -10,6 +14,14 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Callable<Integer> {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
 
     @Override
     public Integer call() throws Exception {
