9f4b94edfaa4cc2dbbb21dab2741291fd6a11a62
alistkov <alistkov.dev@gmail.com>
2026-09-24 22:45:50 +0200

    setup application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
+    application
     id("io.github.ben-manes.versions") version "0.64.0"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass = "hexlet.code.App" }
 
 repositories {
     mavenCentral()
